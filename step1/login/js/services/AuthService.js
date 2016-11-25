e32b40f738091b85eb52676cf8250e8cfdc1f53b
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2016-11-25 12:02:03 +0100

    unstable

--- step1/login/js/services/AuthService.js
+++ step1/login/js/services/AuthService.js
@@ -1,23 +1,57 @@
 angular.module('AuthService', []).service('auth', authFnc);
 
+authFnc.$inject = ['$q'];
+
 function authFnc() {
     var userMap = {};
-    userMap.jdoe = 'jdoepwd'; 
-    userMap.psmith= 'psmithpwd'; 
-    userMap.tp = 'tp';
+
+    userMap.jdoe = {
+        pwd : 'jdoepwd',
+        role : 'member'
+    };
+    userMap.psmith = {
+        pwd : 'psmithpwd',
+        role : 'member'
+    };
+    userMap.tp = {
+        pwd : 'tp',
+        role : 'admin'
+    };
+
+    // var fncContainer = {
+    //     checkUser: checkUser, 
+    //     userList: userList
+
+    // };
+
+    // function checkUser(userlogin, userpwd) { 
+    //     return userMap[userlogin] == userpwd;
+    // };
+
+    // function userList() {
+    //     return userMap;
+    // };
+
+    
+
 
     var fncContainer = {
-        checkUser: checkUser, 
-        userList: userList
+        localAuthAsk: localAuthAsk
     };
     
-    function checkUser(userlogin, userpwd) { 
-        return userMap[userlogin] == userpwd;
-    };
-
-    function userList() {
-        return userMap;
-    };
+    function localAuthAsk(login, pwd) {
+        var deferred = $q.defer(); 
+        setInterval(function (login, pwd) {
+            if ( (userMap[login]) && (userMap[login].pwd == pwd) ) {
+                deferred.resolve();
+            } else {
+                deferred.reject();
+            }
+            clearInterval(this);
+        }, 3000, login, pwd);
+        return deffered.promise;
+    }
 
     return fncContainer;
+
 }
